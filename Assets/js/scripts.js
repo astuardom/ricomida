5e032a65a5231a75aaab98b157c3aa10fd73fcdd
astuardom <73834220+astuardom@users.noreply.github.com>
2024-07-09 11:27:20 -0400

    Subiendo Modificaciones

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -2,12 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
-    // Evento click para el botón "Enviar por correo"
     document.getElementById('enviarCorreo').addEventListener('click', function() {
         alert('El correo fue enviado correctamente...');
     });
 
-    // Evento doble clic para cambiar color de los títulos
     document.getElementById('ingredientes').addEventListener('dblclick', function() {
         this.style.color = 'red';
     });
@@ -16,7 +14,6 @@ document.addEventListener('DOMContentLoaded', function() {
         this.style.color = 'red';
     });
 
-    // Evento click para toggle de tarjetas
     document.querySelectorAll('.card-title').forEach(function(title) {
         title.addEventListener('click', function() {
             const cardText = this.nextElementSibling;
